f4a1292ce31cca187cb3767134ce294db63c413a
Steven <pslopez91@soy.sena.edu.co>
2024-02-21 14:34:29 -1200

    app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+console.log('Hola JavaScript')
+
+//camelCase nombrar metodos, nombres y variables
+
+//PascalCase Identificar clases
+
+//snake case
+//CONSTANTE Mayuscula fija
+
+
+    var numero = 112;
+
+        numero= "Saludo";
+
+            console.log(numero);
+
+                //Para identificar variables:
+                //var 
+                //let
+                //const
+
+
+    var myVariable="15";
+
+    let number=10;
+        number = 10;
+
+            myVariable = 25;
+                console.log(myVariable)
+        number = 22;
+        console.log(number);
+        
+
