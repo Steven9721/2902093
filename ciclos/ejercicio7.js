66cc5e6a10bf8f6e227ec3bf03f37a3819c84332
Steven <julianospina250@gmmail.com>
2024-03-13 23:23:30 -0500

    CICLOS TERMINADOS

--- ciclos/ejercicio7.js
+++ ciclos/ejercicio7.js
@@ -1,20 +1,8 @@
+let numeros = [5, 8, 3, 2, 0]; 
 let suma = 0;
 
-// Obtener los argumentos de la línea de comandos
-const args = process.argv.slice(2);
-
-// Iterar sobre los argumentos y sumar los números
-for (let i = 0; i < args.length; i++) {
-  let numero = parseFloat(args[i]);
-
-  // Verificar si la entrada es 0, en ese caso, salir del bucle
-  if (numero === 0 || isNaN(numero)) {
-    break;
-  }
-
-  // Sumar el número a la suma acumulada
-  suma += numero;
+for (let i = 0; i < numeros.length; i++) {
+    suma += numeros[i];
 }
 
-// Mostrar el resultado
 console.log("La suma de los números ingresados es: " + suma);
